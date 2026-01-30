fe317e911474709085da2eae0f15faae0a86b2f8
dmstn <eunsoo4629@naver.com>
2026-01-30 10:49:57 +0900

    fix2

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/")
     public String home() {
-        return "Homepage 2";
+        return "Homepage 3";
     }
 }
